2e7e584cd42dadf0494aeb752b9d48a49a8df8b3
Wolfpup5456 <38267291+Wolfpup5456@users.noreply.github.com>
2020-11-28 01:38:58 -0600

    Add files via upload

---
+++ Python Year 1/Fratto_Dominick_lab3.py
@@ -0,0 +1,13 @@
+# inputs from user
+Length = input("Enter the length of the room: ")
+Width = input("Enter the width of the room: ")
+
+# telling python these inputs are numbers that can multiply
+Length = int(Length)
+Width = int(Width)
+
+# the equation
+Solution = (Length * Width)
+
+# final result that's output to console
+print("The area of the room:", Solution)
