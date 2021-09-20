d24c89935b179ad465681457920c472af7857f9a
Wolfpup5456 <38267291+Wolfpup5456@users.noreply.github.com>
2021-09-20 04:30:53 -0500

    Delete Python Year 1 directory

--- Python Year 1/Fratto_Dominick_lab6.py
+++
@@ -1,8 +0,0 @@
-for i in range(1, 11):
-    print(i)
-
-status = str("y")
-
-while status == input("Would you like to print this again? (y,n): "):
-    print("Energizer Bunny keeps on going and going... ")
-else: print("he has stopped")
